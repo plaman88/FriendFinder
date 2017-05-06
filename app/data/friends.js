87dda7bebe0cea5f95e1fdf749543a7a5dcbfccf
Patrick <Patrick@Patricks-MacBook-Pro.local>
2017-05-06 00:27:05 -0500

    Finalized FriendFinder App Republished

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+//Starter Friends
+var friends = [
+	{
+		"name": "Todd",
+		"photo": "https://spea.indiana.edu/images/faculty-research/faculty/print-quality/royer_todd.jpg",
+		"scores": [1,1,1,1,1,1,1,1,1,1]
+		
+	},
+	{
+		"name": "Chad",
+		"photo": "https://pbs.twimg.com/profile_images/756902619927437312/nyipM60t.jpg",
+		"scores": [2,2,2,2,2,2,2,2,2,2]
+				
+	}
+];
+
+//Export
+module.exports = friends;
---
+++ app/routing/html-routes.js
@@ -0,0 +1,17 @@
+//app requires path 
+var path = require('path');
+
+// HTML Routes
+// ==============
+
+module.exports = function(app){
+
+	app.get('/', function(req, res){
+		res.sendFile(path.join(__dirname, '/../public/home.html'));
+	});
+
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname, '/../public/survey.html'));
+	});
+
+}
